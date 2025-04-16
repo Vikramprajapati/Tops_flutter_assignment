d1ac936a5f8e48d4109dce77c134b46ccabab852
VIKRAM PRAJAPATI <Vickyprajapati0951@gmail.com>
2025-04-16 17:32:19 +0530

    done

---
+++ lib/assignment/Module1/Q(1)_printInfo.dart
@@ -0,0 +1,16 @@
+void main()
+{
+  String name="Vikram";
+  String DOB="12/06/2002";
+  int age=22;
+  String address="Dindoli,Surat";
+
+  print("Name : $name");
+  print("DOB : $DOB");
+  print("Age : $age");
+  print("Address : $address");
+
+
+
+
+}
